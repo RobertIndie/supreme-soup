ccc760424ce7c60498ace2cbe9a92b5bd3f9c4fa
AaronRobert <Robert.Aaron2017@outlook.com>
2018-03-30 12:19:53 +0800

    algo-2

--- algorithm/ALGO/ALGO-2.cpp
+++ algorithm/ALGO/ALGO-2.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a,ll b)
+{
+    if(a<b)return gcd(b,a);
+    else if(b==0)return a;
+    else return gcd(b,a%b);
+}
+
+ll lcm(ll a,ll b)
+{
+    return a*b/gcd(a,b);
+}
+
 int main()
 {
-    
+    ll n;
+    cin >> n;
+    for(int i = n ;i>0;i--)
+    {
+        for(int j = n;j>0;j--)
+        {
+            for(int k = n;k>0;k--)
+            {
+                if(i!=j&&i!=k&&j!=k)
+                {
+                    cout<<lcm(lcm(i,j),k);
+                    return 0;
+                }
+            }
+        }
+    }
+    return 0;
 }
